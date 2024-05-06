9915b19964c91637e749d47c954522fb43247e1a
sevi-kun <dev@sevi-kun.me>
2024-05-06 18:07:01 +0200

    first try

--- lua/narrator.nvim/init.lua
+++ lua/narrator.nvim/init.lua
@@ -1,7 +1,21 @@
-local function example()
+local opt = {
+    config = {
+        rate = 0,
+        pitch = 0,
+        volume = 0,
+        spell = false,
+    },
+}
+
+local current_line = "Hello world"
+
+local function voice_output(string)
+    vim.cmd.system("spd-say " .. string)
+
   return true
 end
 
+
 return {
-  example = example,
+    voice_output(current_line)
 }
